1052e8e4c6d4295c99bc5e272f033036c3723e8b
AlexStudent2025 <alex.christiansson.student@ksgyf.se>
2025-06-10 20:06:23 +0200

    Skapa själva komprimeringsmodulen

---
+++ komprimering.psm1
@@ -0,0 +1,13 @@
+#Funktion som komprimerar en mapp
+function Compress-Backup {
+    param ($folderToCompress)
+
+    $zipFile = "$folderToCompress.zip"
+
+    try {
+        Compress-Archive -Path "$folderToCompress" -DestinationPath $zipFile -Force
+        Write-Host "Mappen komprimerades till $zipFile"
+    } catch {
+        Write-Host "Kunde inte komprimera mappen!"
+    }
+}
